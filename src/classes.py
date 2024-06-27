1fe69a8d4664465f59b726a9a91df4f147b79960
Gustavo A. Verneck <gustavoverneck@gmail.com>
2024-06-27 17:53:50 -0300

    Add files via upload

--- src/classes.py
+++ src/classes.py
@@ -5,6 +5,7 @@ import multiprocessing
 from datetime import datetime
 import matplotlib.pyplot as plt
 import numpy as np
+from src.setup import main_directory, open_directory_in_file_explorer
 
 class Data:
     def __init__(self, values):
@@ -16,15 +17,17 @@ class Data:
         print(datetime.now(), " - Produzinho gráficos.")
         self.plottingEOS()
         self.plottingTOV()
+        open_directory_in_file_explorer(f"{main_directory}/output")
 
-    def __str__(self):
-        return f"Data({self.values})"
-    
     def plottingEOS(self):
         plt.clf()
         for value in self.data:
             plt.plot(value.e, value.p, label=f"${value.title}$")
+            plt.plot(value.r_max_m, value.max_m, marker=".")
         plt.legend()
+        plt.title("Equations of State")
+        plt.ylabel("$\epsilon$")
+        plt.xlabel("$p$")
         plt.savefig("output/eos.png", dpi=450)
         print(datetime.now(), " - Gráfico das equações de estado foi gerado.")
 
@@ -33,12 +36,17 @@ class Data:
         for value in self.data:
             plt.plot(value.r, value.m, label=f"${value.title}$")
         plt.legend()
+        plt.title("Mass x Radius diagram")
+        plt.xlabel("$Radius$ $(km)$")
+        plt.ylabel("$Mass (M_{\odot})$")
         plt.savefig("output/mr.png", dpi=450)
         print(datetime.now(), " - Gráfico massa-raio foi gerado.")
-    
+
     def plottingPopulation(self):
         pass
     
+    def __str__(self):
+        return f"Data({self.values})"
 
 class DataValues:
     def __init__(self, title):
@@ -49,6 +57,9 @@ class DataValues:
         self.r = []
         self.importData("eos")
         self.importData("tov")
+        self.max_m = max(self.m)
+        self.r_max_m = self.r[self.m.index(self.max_m)]
+
 
     def importData(self, filetype):
         print(datetime.now(), f" - Importando dados {filetype} para csi={self.title}.")
@@ -66,9 +77,9 @@ class DataValues:
                     line2 = line2.split()
                     self.m.append(float(line2[0]))
                     self.r.append(float(line2[1]))
+                del line2
         else:
             raise ValueError("Invalid type.")
-    
 
     def __str__(self):
         return f"Data(csi={self.title})"
--- src/setup.py
+++ src/setup.py
@@ -1,6 +1,5 @@
 # /src/setup.py
-
-import os
+import sys, subprocess, os
 import shutil
 from datetime import datetime
 
@@ -12,13 +11,29 @@ def importValues(filename):
         print(datetime.now(), " - Lendo valores de entrada.")
         return [str(a).split("\n")[0] for a in f.readlines()]
 
+
 def createOutputDir(name="output"):
     if not os.path.exists(f'{main_directory}/{name}'):
         print(datetime.now(), f" - Criando diretório {name}.")
         os.makedirs(f'{main_directory}/{name}')
     
+
 def removeOutputDir(name="output"):
     if os.path.exists(f'{main_directory}/{name}'):
         print(datetime.now(), f" - Removendo diretório {name}.")
         shutil.rmtree(f'{main_directory}/{name}')
 
+
+def open_directory_in_file_explorer(path):
+    if not os.path.isdir(path):
+        raise ValueError(f"The path '{path}' is not a valid directory.")
+    
+    if os.name == 'nt':  # Windows
+        os.startfile(path)
+    elif os.name == 'posix':
+        if sys.platform == 'darwin':  # macOS
+            subprocess.run(['open', path])
+        else:  # Linux
+            subprocess.run(['xdg-open', path])
+    else:
+        raise OSError(f"It was not possible to open output directory in {os.name}.")
